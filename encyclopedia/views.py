7f993f0d8ec63408bbbd2bc0125a368135176414
Ceheiss <cristobal.heiss.j@gmail.com>
2025-04-28 15:47:56 +0200

    rende rmarkdown to HTML

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,3 +1,5 @@
+import markdown2
+
 from django.shortcuts import render
 
 from . import util
@@ -8,3 +10,9 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request, entry):
+    print("check request:", request, entry)
+    html_text = markdown2.markdown(util.get_entry(entry))
+    return render(request, "encyclopedia/entry.html", {
+        "entry": html_text
+    })
